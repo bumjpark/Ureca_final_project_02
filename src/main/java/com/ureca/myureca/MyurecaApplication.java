c4d2cd1da012b8e6c276014288e6e547f62d5b72
박종범 <sfhk199999@gmail.com>
2026-08-07 20:00:33 +0900

    init springboot project

---
+++ src/main/java/com/ureca/myureca/MyurecaApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.myureca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyurecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyurecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ureca/myureca/MyurecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.myureca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyurecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
